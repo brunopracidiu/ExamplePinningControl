da79788b5b6861c072e67e2604adcaa31db1a924
BrunoPracidiu <fo.bruno@gmail.com>
2018-08-30 17:11:35 +1000

    Initial Commit

---
+++ ExamplePinningControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExamplePinningControl
+//
+//  Created by Bruno Ferreira De Oliveira on 30/8/18.
+//  Copyright © 2018 Bruno Ferreira De Oliveira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ExamplePinningControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ExamplePinningControl
+//
+//  Created by Bruno Ferreira De Oliveira on 30/8/18.
+//  Copyright © 2018 Bruno Ferreira De Oliveira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
